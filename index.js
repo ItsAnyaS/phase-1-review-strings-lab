2525a2dd513f4801639da9ef666df4b27195da52
Anastasia <derricksmac@Anyas-MacBook-Air.local>
2022-07-08 09:39:42 -0400

    commit tree 4ee5c69a75fd8bb96ec55088d6fc9e75d6788952

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// Write your code in this file!
+
+let currentUser = 'currentUser'
+let welcomeMessage = `Welcome to Flatbook, ${currentUser}!`
+
+let excitedWelcomeMessage = `WELCOME TO FLATBOOK, ${currentUser}!`
+
+let shortGreeting = `Welcome, ${currentUser[0]}!`
